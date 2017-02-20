c8d6d933a2152d562aa0605049913765641ed41b
Carlos Rodriguez <mundofreakuc@gmail.com>
2017-02-20 21:16:59 +1100

    updated syntax to php 5.6, reformat code to PSR1/PSR-2, fixed problem when the server is windows and the the PID, updated the scheduler worker, renamed the workers in the redis, and more changes

--- lib/ResqueScheduler/InvalidTimestampException.php
+++ lib/ResqueScheduler/InvalidTimestampException.php
@@ -1,12 +1,13 @@
 <?php
+
 /**
-* Exception thrown whenever an invalid timestamp has been passed to a job.
-*
-* @package		ResqueScheduler
-* @author		Chris Boulton <chris@bigcommerce.com>
-* @copyright	(c) 2012 Chris Boulton
-* @license		http://www.opensource.org/licenses/mit-license.php
-*/
+ * Exception thrown whenever an invalid timestamp has been passed to a job.
+ *
+ * @package        ResqueScheduler
+ * @author        Chris Boulton <chris@bigcommerce.com>
+ * @copyright    (c) 2012 Chris Boulton
+ * @license        http://www.opensource.org/licenses/mit-license.php
+ */
 class ResqueScheduler_InvalidTimestampException extends Resque_Exception
 {
 
